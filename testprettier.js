1faa5bbc2c1d69df17d623faecf8f691449269c6
Augustin Bukin <94466026+Nehocute@users.noreply.github.com>
2024-03-29 13:29:14 +0100

    Create testprettier.js

---
+++ testprettier.js
@@ -0,0 +1,6 @@
+// This JavaScript file intentionally violates Prettier recommendations
+
+function   exampleFunction   (   )   { 
+console.log('This function has improper spacing and indentation.');
+  console.log('It violates Prettier recommendations.');
+}
