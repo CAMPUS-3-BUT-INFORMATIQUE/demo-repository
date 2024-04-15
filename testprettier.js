e9e0e3659971c1fb7d679168d82a869f19e1a9fe
Théo <theo.lebiez@ayctor.com>
2024-04-15 14:31:54 +0200

    test prettier fix

--- testprettier.js
+++ testprettier.js
@@ -4,3 +4,9 @@ function exampleFunction() {
   console.log("This function has improper spacing and indentation.");
   console.log("It violates Prettier recommendations.");
 }
+
+
+function exampleFunction2() {console.log("This function has improper spacing and indentation.");
+  
+                   console.log("It violates Prettier recommendations.");
+}
