436e80122beea2fdd0842bd89994c755e4bf2486
Deeffault <Deeffault@users.noreply.github.com>
2024-04-15 12:37:20 +0000

    Prettified Code!

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./resources/views/**/*.{html,js,php}", "./resources/js/**/*.{js}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [require('daisyui')],
-}
-
+    content: [
+        "./resources/views/**/*.{html,js,php}",
+        "./resources/js/**/*.{js}",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [require("daisyui")],
+};
